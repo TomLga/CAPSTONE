6992e1363778b6df06d735cc14b51a38dba7f2eb
TomLga <semitallbox@gmail.com>
2023-08-31 16:28:32 +0200

    added cards on prod page

---
+++ vuefront/src/views/ProductsView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+<div class="mainDiv">
+
+        
+        <h1 id="prodText">PRODUCTS</h1>
+        <ProdCard/>
+</div>
+    </div>
+</template>
+
+<script>
+import ProdCard from '@/components/ProdCard.vue'
+
+
+    export default {
+        components:{
+            ProdCard
+        }
+        
+    }
+</script>
+<style>
+#prodText{
+    padding-top: 100px;
+}
+
+</style>
+
