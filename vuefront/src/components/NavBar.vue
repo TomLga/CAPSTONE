4097c9a9481b0379eb8c48626d4cc251626d9fdb
TomLga <semitallbox@gmail.com>
2023-09-01 13:00:31 +0200

    login done

--- vuefront/src/components/NavBar.vue
+++ vuefront/src/components/NavBar.vue
@@ -30,6 +30,9 @@
                   <li class="nav-item">
                     <a class="nav-link" href="/about">Cart</a>
                   </li>             
+                  <li class="nav-item">
+                    <a class="nav-link" href="/resgister">Register</a>
+                  </li>             
 
                 </ul>
                 <form class="d-flex" role="search">
---
+++ vuefront/src/components/SignupComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 id="toptxt">SIGN UP</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+#toptxt{
+    padding-top: 100px;
+}
+
+</style>
