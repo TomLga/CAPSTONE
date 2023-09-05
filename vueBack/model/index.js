762123977e598c04028c22025b32cf8bdb404b5b
TomLga <semitallbox@gmail.com>
2023-09-05 12:01:31 +0200

    setting up history

--- vueBack/model/index.js
+++ vueBack/model/index.js
@@ -2,11 +2,13 @@
 const Users = require('./Users')
 const products = require('./Products')
 const Category = require('./category')
+const History = require('./History')
 
 // Export all objects
 module.exports = {
     users: new Users(),
     products:new products(),
-    category: new Category()
+    category: new Category(),
+    history: new History()
 }
 
