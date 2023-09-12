5f86b327255e24b93aa101698b86c111d56644da
Tomlga <semitallbox@gmail.com>
2023-09-13 00:24:10 +0200

    cart 50% work

--- vuefront/src/components/ProdCard.vue
+++ vuefront/src/components/ProdCard.vue
@@ -1,35 +1,34 @@
 <template>
+  <div>
+    <form class="d-flex searchTab" role="search" @submit.prevent="searchProducts">
+      <input v-model="searchQuery" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success btnSearch" type="submit">Search</button>
+    </form>
 
-  <form class="d-flex searchTab" role="search" @submit.prevent="searchProducts">
-    <input v-model="searchQuery" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success btnSearch" type="submit">Search</button>
-  </form>
+    <div class="card-group prodCardBody">
+      <div class="row">
+        <div class="col-md-6 col-lg-4" v-for="item in filteredProducts" :key="item.prodID">
+    
+          <div class="card wholeCard">
+            <img :src="item.prodImg" class="card-img-top img-fluid" style="width: 22rem; height: 22rem; margin: 10px auto;">
+            <div class="card-body prodCards">
+              <h2 class="card-title">{{ item.prodName }}</h2>
+            </div>
 
-  <div class="card-group prodCardBody">
-    <div class="row" v-if="filteredProducts.length > 0">
-      <div class="col-md-6 col-lg-4" v-for="item in filteredProducts" :key="item.prodID">
-        <!-- Your card content here -->
-        <div class="card wholeCard">
-          <img :src="item.prodImg" class="card-img-top img-fluid" style="width: 22rem; height: 22rem; margin: 10px auto;">
-          <div class="card-body prodCards">
-            <h2 class="card-title">{{ item.prodName }}</h2>
-          </div>
+            <div id="btncartmore">
+              <button class="btn btn-outline-primary" @click="viewItem(item.prodID)">VIEW MORE</button>
+              <!-- <button class="btn btn-outline-primary" @click="AddToCart(item.prodID)">Add to Cart</button> -->
+              <button @click="AddCart(item)" class="btn">Cart</button>
+             
+            </div>
 
-          <div id="btncartmore">
-    <button class="btn btn-outline-primary" @click="viewItem(item.prodID)" >VIEW MORE</button>
-
-            <router-link to="/viewMore" class="btn btn-outline-primary">CART</router-link>
-          </div>
-
-
-          <div class="card-footer">
-            <p class="priceDiv"> R {{ item.price }}</p>
+            <div class="card-footer">
+              <p class="priceDiv"> R {{ item.price }}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <SpinnerComp />
+      
     </div>
   </div>
 </template>
@@ -59,23 +58,33 @@ export default {
   },
   methods: {
     searchProducts() {
+     
     },
-    viewItem(prodID){
-      const cProd = this.products.find(
-        (item)=>
-        item.prodID === prodID
-      );
-      this.$store.commit("setSignalView", cProd);
-      this.$router.push({name: "prodSingView", params:{ prodID:prodID}})
-    }
+    viewItem(prodID) {
+    const selectedProduct = this.products.find((item) => item.prodID === prodID);
+    this.$store.dispatch('addToCart', selectedProduct);
+    this.$router.push({ name: 'prodSingView', params: { prodID: prodID } });
+  },
+  // addToCart(prodID) {
+  //   const selectedProduct = this.products.find((item) => item.prodID === prodID);
+  //   this.$store.dispatch('addToCart', selectedProduct);
+  // }
+  AddCart(item) {
+      const data = JSON.parse(localStorage.getItem('cart')) || []
+
+      const newData = {key: item}
+      data.push(newData)
+
+      localStorage.setItem('cart', JSON.stringify(data))
+    },
+
+
   },
   mounted() {
     this.$store.dispatch('fetchAllProducts');
   },
 };
 </script>
-
-
 
 <style scoped>
 .prodCardBody {
@@ -86,82 +95,48 @@ export default {
 .wholeCard {
   background: #111010c5;
   color: rgb(255, 254, 254);
-  margin: 20px; /* Adjust the margin between cards */
+  margin: 20px;
+  padding: 15px;
+  max-width: 100%;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 #btncartmore {
   display: flex;
   justify-content: space-evenly;
 }
-.priceDiv{
+.priceDiv {
   font-size: 20px;
 }
-
 /*hover effects*/
- 
 img {
   transition: transform 1s ease-in-out, border 1s ease-in-out;
-  
 }
-
 img:hover {
   transform: scale(1.2);
   border: 7px solid rgb(0, 0, 0);
   border-radius: 20px;
 }
-
-
-.searchTab{
+.searchTab {
   width: 50%;
   margin: auto;
   margin-bottom: 20px;
-
-
 }
-.searchTab> input{
+.searchTab > input {
   background: #706767;
   color: white;
 }
-
 @media (max-width: 600px) {
-  .wholeCard{
+  .wholeCard {
     width: 400px;
-  
   }
-
-}
-
-
-.wholeCard {
-  background: #111010c5;
-  color: rgb(255, 254, 254);
-  margin: 20px; /* Adjust the margin between cards */
-
-  /* Add padding and set a maximum width to control the card's size */
-  padding: 15px;
-  max-width: 100%; /* Allow the card to take up the full width of its container */
-
-  /* Add a box shadow for a card-like effect */
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-
-  /* Add border-radius for rounded corners */
-  border-radius: 10px;
-
-  /* Center the card content horizontally */
-  margin-left: auto;
-  margin-right: auto;
-
-  /* Add some spacing between the card's elements */
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-@media (max-width: 600px) {
-  form{
-  width: 100%;
-  
+  form {
+    width: 100%;
   }
 }
 </style>
-
-
-
--- vuefront/src/views/CartView.vue
+++ vuefront/src/views/CartView.vue
@@ -1,40 +1,88 @@
 <template>
+  <div>
+    <h1 style="margin-top: 100px;">Shopping Cart</h1>
     <div>
-        <h1 id="ckot">CHECKOUT</h1>
-        <table class="table">
-            <thead>
-              <tr>
-                  <th>prodImg</th>
-                <td>#ID</td>
-                <th>Name</th>
-                <th>qty</th>
-                <th>price</th>
-                <th>Total</th>
-              
-        
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                  <td><img src="item.prodImg" class="card-img-top img-fluid" style="width: 5rem; height: 5rem; margin:auto;"></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-             
-              </tr>
-            </tbody>
-          </table>
 
+      <table class="cart-table" v-if="cart">
+        <thead>
+          <tr>
+            <th>#ID</th>
+            <th>Product Image</th>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Unit Price</th>
+            <th>remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in cart" :key="product.prodID">
+            <td>{{ product.key.prodID }}</td>
+            <td>{{ product.key.prodImg }}</td>
+            <td>{{ product.key.prodName }}</td>
+            <td>{{ product.key.qty }}</td>
+            <td>{{ product.key.price }}</td>
+            <td><button @click="delProduct(product.key.prodID)">🗑️</button></td>
+          </tr>
+        </tbody>
+      </table>
+    <div v-else>
+            <img src="https://i.postimg.cc/8P7MdZmC/empty-Shop-Cart.png">
+        </div>
     </div>
+
+  </div>
 </template>
 
+<script>
+import axios from 'axios'
+export default {
+  computed: {
+    cart() {
+      const cartData = JSON.parse(localStorage.getItem("cart"));
+      return cartData;
+    },
+  },
+  methods: {
+    delProduct(prodID){
+      console.log(prodID);
 
+      if(confirm('are you sure?')){
+   
+      axios.delete(`https://capstoneswordall.onrender.com/product/${prodID}`)
+      .then(res =>{
+        alert(res.data.msg)
+    
+
+      })
+      }
+
+    },
+  }
+};
+</script>
 
 <style>
-#ckot{
-    padding-top: 100px;
+/* Add your custom styles here */
+.cart-table {
+  margin-top: 20px;
+  border-collapse: collapse;
+  width: 100%;
 }
 
-</style> 
+.cart-table th,
+.cart-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.cart-table th {
+  background-color: #c7a4a4;
+}
+
+#checkout-heading {
+  font-size: 24px;
+  margin-top: 20px;
+}
+
+</style>
